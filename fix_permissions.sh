976cf6d3106c6aaa11ca05cd2dd03b367ca94280
Kirk Bauer <kirk@neadwerx.com>
2015-12-04 16:29:23 -0500

    fix perms

---
+++ fix_permissions.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+chmod -R a+rX . *
+chmod a+rwx cache images logs media pub includes/languages/english/html_includes
+chmod a+rwx includes/languages/english/html_includes/classic zenadmin/backups
+chmod a+rwx zenadmin/images/graphs
+
