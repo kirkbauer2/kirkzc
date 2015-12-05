87099a76f96a071fffd0b32b25f72c11edd72dfc
Kirk Bauer <kirk@neadwerx.com>
2015-12-04 23:28:30 -0500

    updates

---
+++ includes/languages/english/yorkshire_clean/header.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @package languageDefines
+ * @copyright Copyright 2003-2006 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: header.php 2940 2006-02-02 04:29:05Z drbyte $
+ */
+
+// header text in includes/header.php
+  define('HEADER_TITLE_CREATE_ACCOUNT', 'Create Account');
+  define('HEADER_TITLE_MY_ACCOUNT', 'My Account');
+  define('HEADER_TITLE_CART_CONTENTS', 'Shopping Cart');
+  define('HEADER_TITLE_CHECKOUT', '<span class="header-checkout">Checkout</span>');
+  define('HEADER_TITLE_TOP', 'Top');
+  define('HEADER_TITLE_CATALOG', 'Home');
+  define('HEADER_TITLE_LOGOFF', 'Log Out');
+  define('HEADER_TITLE_LOGIN', 'Log In');
+  define('HEADER_TITLE_PROMO1', '<span class="promo-big"></span>');
+
+// definiitions for header icons
+  define('HEADER_ICON_HOME', 'home.png');
+  define('HEADER_ICON_LOGOFF', 'logoff.png');
+  define('HEADER_ICON_ACCOUNT', 'account.png');
+  define('HEADER_ICON_LOGIN', 'login.png');
+  define('HEADER_ICON_CART', 'cart.png');
+  define('HEADER_ICON_CHECKOUT', 'checkout.png');
+
+
+// added defines for header alt and text
+  define('HEADER_ALT_TEXT', 'boardgamestorge.com');
+  define('HEADER_SALES_TEXT', ' ');
+  define('HEADER_LOGO_WIDTH', '235px');
+  define('HEADER_LOGO_HEIGHT', '60px');
+  define('HEADER_LOGO_IMAGE', 'logo.png');
+
+// header Search Button/Box Search Button
+  define('HEADER_SEARCH_BUTTON','Search');
+  define('HEADER_SEARCH_DEFAULT_TEXT', 'Enter search keywords here');
+
+?>
